eae320d3183acea1a4777e61399881b919603cdb
mhdta23 <85657560+mhdta23@users.noreply.github.com>
2021-08-24 22:50:45 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -11,7 +11,7 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
     
     
    
-  r_text[0] = "https://www.pixelstalk.net/wp-content/uploads/2016/03/MRTECH-BOT-wallpaper-HD-dowload.jpg";
+  r_text[0] = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9UeuCBFwesPWwdcV2Sv_HoZXrNiCZGy-aNg&usqp=CAU";
     
     
     var i = Math.floor(1*Math.random())
