bc780c4317688ec9df1d8f060b4b99a1a1ee143c
mhdta23 <85657560+mhdta23@users.noreply.github.com>
2021-08-24 22:35:37 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -10,7 +10,7 @@ Asena.addCommand({pattern: 'menu', fromMe: false, desc: Lang.WP}, (async (messag
     var r_text = new Array ();
     
     
-    r_text[0] = "https://pin.it/1088kyV";
+    r_text[0] = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9UeuCBFwesPWwdcV2Sv_HoZXrNiCZGy-aNg&usqp=CAU";
     
     
     var i = Math.floor(1*Math.random())
