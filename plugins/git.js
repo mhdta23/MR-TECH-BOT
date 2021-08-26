2734e2b783de8ff6701eca6f3b0aedc086f477a3
mhdta23 <85657560+mhdta23@users.noreply.github.com>
2021-08-26 11:34:52 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -11,7 +11,7 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
     
     
    
-  r_text[0] = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9UeuCBFwesPWwdcV2Sv_HoZXrNiCZGy-aNg&usqp=CAU";
+  r_text[0] = "https://i.ibb.co/xz3DVT3/mrtechbot.jpg";
     
     
     var i = Math.floor(1*Math.random())
